3346958276073a9526a8233916b21f5061279492
Josh Lim <email": null>
2017-06-02 11:22:59 -0700

    Jquery

---
+++ app/assets/javascripts/invoice.js
@@ -0,0 +1,18 @@
+var ready = function() {
+
+    $(document).on("click", "tr[data-link]", function() {
+        window.location = $(this).data('link');
+    });
+
+    $(document).on("change", "#hi", function() {
+      // console.log($(this).attr('action'))
+      var object = $(this).serialize()
+      // console.log($(this).serialize())
+      window.location = $(this).attr('action') + '?' + object
+    });
+
+};
+
+
+$(document).ready(ready)
+$(document).on("turbolinks:load", ready)
