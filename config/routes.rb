ebbf2d5041420165ecae48095db8ec2c125b3a4a
Josh Lim <email": null>
2017-06-01 18:27:28 -0700

    Delete unnecessary stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root to: 'invoices#index'
-
-
   resources :invoices
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
